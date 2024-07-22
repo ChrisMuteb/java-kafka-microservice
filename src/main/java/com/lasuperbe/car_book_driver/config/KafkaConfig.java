0c77ed8f920abcbac66bfcfe262e6426d9ddc1d0
chriskayomb <ibond.chris@gmail.com>
2024-07-22 10:39:44 +0200

    2 microservices(driver & user). Driver publishes its latitude & longitude into Kafka. The user consumes those data.

---
+++ src/main/java/com/lasuperbe/car_book_driver/config/KafkaConfig.java
@@ -0,0 +1,15 @@
+package com.lasuperbe.car_book_driver.config;
+
+import com.lasuperbe.car_book_driver.constant.AppConstant;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfig {
+    @Bean
+    public NewTopic topic(){
+        return TopicBuilder.name(AppConstant.CAB_LOCATION).build();
+    }
+}
